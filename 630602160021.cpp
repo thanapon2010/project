b25540b9ed65cd35ddeb1d31aff6b7e84bf06c58
thanapon2010 <68541484+thanapon2010@users.noreply.github.com>
2020-07-20 17:50:31 +0700

    Add files via upload
    นาย ธนพล ชานุวัจน์ 6306021630021 sec a

---
+++ 630602160021.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << endl;
+    cout << " 1st Term of 2020        \\45132114   Wanthanee Prachuabsupakij" << endl;
+    cout << endl;
+    cout << "--------------------------------------------------------------------------\n";
+    cout << "| Subject ID | Subject name               | Score (100)| Grad |\n";
+    cout << "--------------------------------------------------------------------------\n";
+    cout << "|  4570034   | Inteoduction to computer    |     85     | \"A\"  |\n";
+    cout << "|  4580247   | English                     |     74     | \"B\"  |\n";
+    cout << "|  4530001   | Design Thinking             |     92     | \"A\"  |\n";
+    cout << "--------------------------------------------------------------------------\n";
+    cout << "|   TOTAL    | Subject = 3                 | Average = 3.87 |\n";
+    cout << "====================================*===\n";
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << "KMUTNB";
+}
